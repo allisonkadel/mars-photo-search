be7ddeb92568f4d8a3e25c155614d407f5d33769
allisonkadel <akadel01@gmail.com>
2019-06-22 09:53:45 -0400

    change App component from functional to class

--- mars-photo-search-client/src/App.js
+++ mars-photo-search-client/src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import SearchForm from './components/SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Curious About Mars?</h1>
-      <h2>Search for photos taken by NASA's Curiosity Mars rover.</h2>
-      <SearchForm />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Curious About Mars?</h1>
+        <h2>Search for photos taken by NASA's Curiosity Mars rover.</h2>
+        <SearchForm />
+      </div>
+    );
+  }
 }
 
 export default App;

4836ef7b3f13c4bcff92c5cd24aaf76bb1cff5bb
allisonkadel <akadel01@gmail.com>
2019-06-22 12:23:51 -0400

    build query string fetch request to send to rails api and add faraday gem to start building api request in search action

--- mars-photo-search-api/app/controllers/api/photos_controller.rb
+++ mars-photo-search-api/app/controllers/api/photos_controller.rb
@@ -1,5 +1,12 @@
-class PhotosController < ApplicationController
+class Api::PhotosController < ApplicationController
+
+  API_URL = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos'
+
   def search
-    binding.pry
+    resp = Faraday.get API_URL do |req|
+      req.params['sol'] = params['sol']
+      req.params['camera'] = params['camera']
+    end
+    byebug
   end
 end
--- mars-photo-search-client/src/components/SearchForm.js
+++ mars-photo-search-client/src/components/SearchForm.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class SearchForm extends Component {
 
-  API_URL = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos'
+  RAILS_API_URL = 'http://localhost:3000/api/photos'
 
   state = {
     sol: '',
@@ -13,16 +13,28 @@ class SearchForm extends Component {
 
     event.preventDefault();
 
-    fetch('https://localhost:3000/api/search')
+    let { sol, camera } = this.state
+    let params = { sol:sol, camera:camera }
+    let url = new URL(this.RAILS_API_URL)
+    url.search = new URLSearchParams(params)
 
-    // let { sol, camera } = this.state
-    // let params = { sol:sol, camera:camera }
-    // let url = new URL(this.API_URL)
-    // url.search = new URLSearchParams(params)
-    //
-    // fetch(url)
+    fetch(url)
+    .then(response => response.json())
+    .then(photos => console.log(photos))
+    .catch(error => console.log(error))
+
+    // fetch(this.API_URL, {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'params': {
+    //       'sol': this.state.sol,
+    //       'camera': this.state.camera
+    //     }
+    //   }
+    // })
     // .then(response => response.json())
-    // .then(photos => console.log(photos))
+    // .then(data => console.log(data))
     // .catch(error => console.log(error))
 
   }

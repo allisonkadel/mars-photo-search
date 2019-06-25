9487d4ebd6cdd46ad7e36dce02b7f540f8dbb853
allisonkadel <akadel01@gmail.com>
2019-06-25 07:48:19 -0400

    add Photos component and render in App

--- mars-photo-search-client/src/App.js
+++ mars-photo-search-client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchForm from './components/SearchForm';
+import Photos from './components/Photos';
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
         <h1>Curious About Mars?</h1>
         <h2>Search for photos taken by NASA's Curiosity Mars rover.</h2>
         <SearchForm />
+        <Photos />
       </div>
     );
   }
---
+++ mars-photo-search-client/src/components/Photos.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Photos extends Component {
+
+
+
+  render() {
+    return (
+      <h1>photos component</h1>
+    )
+  }
+}
+
+export default Photos;

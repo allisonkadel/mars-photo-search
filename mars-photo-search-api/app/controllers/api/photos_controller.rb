74caf0d65ca8eb4d26ff34d6cb3128a42c8659c7
allisonkadel <akadel01@gmail.com>
2019-06-25 10:41:25 -0400

    intall rspec-rails and factory bot gems

--- mars-photo-search-api/app/controllers/api/photos_controller.rb
+++ mars-photo-search-api/app/controllers/api/photos_controller.rb
@@ -1,9 +1,9 @@
 class Api::PhotosController < ApplicationController
 
-  API_URL = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos'
+  MARS_API_URL = 'https://mars-photos.herokuapp.com/api/v1/rovers/curiosity/photos'
 
   def search
-    resp = Faraday.get API_URL do |req|
+    resp = Faraday.get MARS_API_URL do |req|
       req.params['sol'] = params['sol']
       req.params['camera'] = params['camera']
     end
@@ -14,7 +14,9 @@ class Api::PhotosController < ApplicationController
 
 end
 
-# Where we left off:
+# Things left to do:
 
-# built Faraday request and added gem to gemfile, but get a NameError
-# left off on minute 33 in Learn Instruct video
+# Clear form after Submit
+# Display header describing results or Display 'no results found' message
+# build server side validation
+# write test suite
